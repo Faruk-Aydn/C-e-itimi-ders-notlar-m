5ccebedb1533347ea21a05edcc3780dd6f4eb7ca
FARUK <farukaydin8245@gmail.com>
2024-10-02 16:26:14 +0300

    Add project files.

---
+++ 01_MainSubjects/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_MainSubjects
+{
+    internal class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            #region Yazdırma komutları
+            //Console.WriteLine("***** YEMEK KATEGORİLERİ *****");
+            //Console.WriteLine("");
+            //Console.WriteLine("1-ÇORBALAR");
+            //Console.WriteLine("2-ANA YEMEK ");
+            //Console.WriteLine("3-SOĞUK BAŞLANGIÇ");
+            //Console.WriteLine("4-SALATALAR");
+            //Console.WriteLine("5-İÇECEKLER");
+            //Console.WriteLine("6-TATLILAR");
+            //Console.WriteLine("");
+            //Console.WriteLine("***** YEMEK KATEGORİLERİ *****");
+            #endregion
+
+            #region String  Değişkenler
+
+            //String costumerName;
+            //String costumerSurname;
+            //String costumerPhone;
+            //String costumerEmail,district,city;
+
+            //costumerName = "Ali";
+            //costumerSurname = " Yılmaz";
+            //costumerPhone = "0560 780 90 50";
+            //costumerEmail = "dneme123@gmail.com";
+            //district = " dereli";
+            //city = "giresun ";
+
+
+            //Console.WriteLine(" ***** MÜŞTERİ REZERVASYON *****");
+            //Console.WriteLine("");
+            //Console.WriteLine("------------------------------------");
+            //Console.WriteLine("Müsteri: "+ costumerName+" "+ costumerSurname);
+            //Console.WriteLine("İletişim: "+ costumerPhone);
+            //Console.WriteLine("Email Adresi: " + costumerEmail);
+            //Console.WriteLine("Adres: "+ district+ "/" + city);
+            //Console.WriteLine("------------------------------------");
+
+
+
+
+
+
+            #endregion
+
+
+
+
+
+            Console.Read();
+
+
+        }
+    }
+}
---
+++ 02_Variables/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Double Değişkenler
+
+            //double number;
+            //number = 3.27;
+            //Console.WriteLine(number);
+            Console.WriteLine("***** Fiyat Listesi *****");
+            double applePirece, orrangePirece, bananaPirece, limePirece;
+            applePirece = 23.56;
+            orrangePirece = 15.44;
+            bananaPirece = 10;
+            limePirece = 12.9;
+
+            Console.WriteLine("");
+            Console.WriteLine("----- Elma Birim Fiyatı : " + applePirece + " tl") ;
+            Console.WriteLine("----- Portakal Birim Fiyatı : " +  orrangePirece + " tl");
+            Console.WriteLine("----- muz Birim Fiyatı : " +  bananaPirece + " tl");
+            Console.WriteLine("----- lime Birim Fiyatı : " + limePirece + " tl");
+            Console.WriteLine();
+
+            Console.WriteLine("***** Fiyat Listesi *****");
+            Console.WriteLine();
+
+            double appelGram,orrangeGram,bananaGram, limeGram;
+            appelGram = 1.333;
+            orrangeGram = 2.234;
+            bananaGram = 4.124;
+            limeGram = 3.98;
+
+            double totalAppelPrice = applePirece * appelGram;
+            double totalOrrangePrice = orrangePirece* orrangeGram;
+            double totalBananaPrice = bananaPirece* orrangeGram;
+            double totalLimePrice = limeGram* orrangeGram;
+
+            Console.WriteLine("Elmanın Toplam Fiyatı : " + totalAppelPrice);
+            Console.WriteLine("Portakalın Toplam Fiyatı : " + totalOrrangePrice);
+            Console.WriteLine("Muzun Toplam Fiyatı : " + totalBananaPrice);
+            Console.WriteLine("Lime Toplam Fiyatı : " + totalLimePrice);
+            double shoppingTotalPrice = totalAppelPrice + totalBananaPrice + totalLimePrice + totalOrrangePrice;
+            Console.WriteLine("");
+            Console.WriteLine();
+            Console.WriteLine("Alışveriş Toplam Tutar : " + shoppingTotalPrice +"tl");
+
+          
+        
+
+
+
+
+
+            #endregion
+
+            Console.Read();
+
+        }
+    }
+}
